5fe2fba532917cc3f2397870b34190e90f2ffb9b
markcarey <mark@markcarey.com>
2023-09-28 13:22:27 -0400

    erc777 receiver

--- contracts/StreamStaker.sol
+++ contracts/StreamStaker.sol
@@ -5,3 +5,40 @@ pragma solidity 0.8.19;
 import {
     ISuperToken
 } from "@superfluid-finance/ethereum-contracts/contracts/interfaces/superfluid/ISuperfluid.sol";
+
+import "@openzeppelin/contracts-upgradeable/token/ERC777/IERC777Upgradeable.sol";
+import { IERC1820RegistryUpgradeable } from "@openzeppelin/contracts-upgradeable/utils/introspection/IERC1820RegistryUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC777/IERC777RecipientUpgradeable.sol";
+
+contract StreamStaker is IERC777RecipientUpgradeable {
+    IERC1820RegistryUpgradeable private constant ERC1820 =
+        IERC1820RegistryUpgradeable(
+            0x1820a4B7618BdE71Dce8cdc73aAB6C95905faD24
+        );
+
+    ISuperToken private _superToken;
+
+    function initialize(address superToken) public initializer {
+        _superToken = ISuperToken(superToken);
+        ERC1820.setInterfaceImplementer(
+            address(this),
+            keccak256("ERC777TokensRecipient"),
+            address(this)
+        );
+    }
+
+    function onTokenReceived(
+        address operator,
+        address from,
+        address to,
+        uint256 amount,
+        bytes calldata userData,
+        bytes calldata operatorData
+    ) external override returns (bytes memory) {
+        return "";
+    }
+
+    function getSuperToken() public view returns (ISuperToken) {
+        return _superToken;
+    }
+}
