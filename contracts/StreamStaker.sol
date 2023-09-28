4b230be4f7746e58d25b1261650ee3af08345e0a
markcarey <mark@markcarey.com>
2023-09-28 13:12:44 -0400

    start contract

---
+++ contracts/StreamStaker.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.19;
+
+import {
+    ISuperToken
+} from "@superfluid-finance/ethereum-contracts/contracts/interfaces/superfluid/ISuperfluid.sol";
