a0ba13a37fc3fd26cf89bab10b3b18550082a16a
markcarey <mark@markcarey.com>
2023-09-28 14:38:49 -0400

    basic contract done

--- contracts/StreamStaker.sol
+++ contracts/StreamStaker.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: MIT
 
 pragma solidity 0.8.19;
+pragma abicoder v2;
 
 import {
     ISuperToken
@@ -12,30 +13,46 @@ import { IERC1820RegistryUpgradeable } from "@openzeppelin/contracts-upgradeable
 import "@openzeppelin/contracts-upgradeable/token/ERC777/IERC777RecipientUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/utils/ReentrancyGuardUpgradeable.sol";
 
+import '@uniswap/v3-periphery/contracts/interfaces/ISwapRouter.sol';
+import '@uniswap/v3-periphery/contracts/libraries/TransferHelper.sol';
+
 contract StreamStaker is IERC777RecipientUpgradeable, ReentrancyGuardUpgradeable {
     IERC1820RegistryUpgradeable private constant ERC1820 =
         IERC1820RegistryUpgradeable(
             0x1820a4B7618BdE71Dce8cdc73aAB6C95905faD24
         );
 
-    ISuperToken private _superToken;
-    ERC20 private _lsdToken;
+    ISuperToken private USDbCx = ISupertoken(0x4dB26C973FaE52f43Bd96A8776C2bf1b0DC29556);
+    IERC20 private USDbC = IERC20(0xd9aAEc86B65D86f6A7B5B1b0c42FFA531710b6CA);
+    IERC20 private cbETH = IERC20(0x2Ae3F1Ec7F1F5012CFEab0185bfc7aa3cf0DEc22);
+    IERC20 private WETH = IERC20(0x4200000000000000000000000000000000000006);
+
+    ISwapRouter swapRouter = ISwapRouter(0x2626664c2603336E57B271c5C0b26F421741e481);
 
     function initialize(address owner, address superToken, address lsdToken) public initializer {
         __ReentrancyGuard_init();
-        _superToken = ISuperToken(superToken);
-        _lsdToken = ERC20(lsdToken);
         ERC1820.setInterfaceImplementer(
             address(this),
             keccak256("ERC777TokensRecipient"),
             address(this)
         );
+        USDbC.approve(address(swapRouter), 2**256 - 1);
     }
 
     function stake() external nonReentrant {
         _superToken.downgrade(_superToken.balanceOf(address(this)));
         //_superToken.getUnderlyingToken()
+        ISwapRouter.ExactInputParams memory params =
+            ISwapRouter.ExactInputParams({
+                path: abi.encodePacked(USDbC, 500, WETH, 500, cbETH),
+                recipient: owner,
+                deadline: block.timestamp,
+                amountIn: USDbC.balanceOf(address(this)),
+                amountOutMinimum: 0
+            });
 
+        // Executes the swap.
+        amountOut = swapRouter.exactInput(params);
     }
 
     function onTokenReceived(
