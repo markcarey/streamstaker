8a9ec69fc7266e2e391f7243fabdcee63fe2da7a
markcarey <mark@markcarey.com>
2023-09-28 13:54:20 -0400

    progress

--- contracts/StreamStaker.sol
+++ contracts/StreamStaker.sol
@@ -5,26 +5,37 @@ pragma solidity 0.8.19;
 import {
     ISuperToken
 } from "@superfluid-finance/ethereum-contracts/contracts/interfaces/superfluid/ISuperfluid.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 import "@openzeppelin/contracts-upgradeable/token/ERC777/IERC777Upgradeable.sol";
 import { IERC1820RegistryUpgradeable } from "@openzeppelin/contracts-upgradeable/utils/introspection/IERC1820RegistryUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/token/ERC777/IERC777RecipientUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/utils/ReentrancyGuardUpgradeable.sol";
 
-contract StreamStaker is IERC777RecipientUpgradeable {
+contract StreamStaker is IERC777RecipientUpgradeable, ReentrancyGuardUpgradeable {
     IERC1820RegistryUpgradeable private constant ERC1820 =
         IERC1820RegistryUpgradeable(
             0x1820a4B7618BdE71Dce8cdc73aAB6C95905faD24
         );
 
     ISuperToken private _superToken;
+    ERC20 private _lsdToken;
 
-    function initialize(address superToken) public initializer {
+    function initialize(address owner, address superToken, address lsdToken) public initializer {
+        __ReentrancyGuard_init();
         _superToken = ISuperToken(superToken);
+        _lsdToken = ERC20(lsdToken);
         ERC1820.setInterfaceImplementer(
             address(this),
             keccak256("ERC777TokensRecipient"),
             address(this)
         );
+    }
+
+    function stake() external nonReentrant {
+        _superToken.downgrade(_superToken.balanceOf(address(this)));
+        //_superToken.getUnderlyingToken()
+
     }
 
     function onTokenReceived(
