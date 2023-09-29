93d26893f830901b96f153a477b3795682ddec00
markcarey <mark@markcarey.com>
2023-09-29 15:51:50 -0400

    functions start

--- server/functions/index.js
+++ server/functions/index.js
@@ -1,19 +1,8 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * const {onCall} = require("firebase-functions/v2/https");
- * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
+global.__base = __dirname + '/';
+const functions = require("firebase-functions");
 
-const {onRequest} = require("firebase-functions/v2/https");
-const logger = require("firebase-functions/logger");
+var staker = require(__base + 'staker');
 
-// Create and deploy your first functions
-// https://firebase.google.com/docs/functions/get-started
-
-// exports.helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+exports.indexer = functions.pubsub.schedule('every 5 minutes').onRun((context) => {
+    return staker.indexer(context);
+}); // indexer
---
+++ server/functions/staker/index.js
@@ -0,0 +1,211 @@
+var firebase = require('firebase-admin');
+if (!firebase.apps.length) {
+    firebase.initializeApp();
+}
+
+const express = require("express");
+const api = express();
+
+const { ethers } = require("ethers");
+
+const factoryJSON = require(__base + 'staker/StreamStakerFactory.json');
+const stakerJSON = require(__base + 'staker/StreamStaker.json');
+
+const fetch = require('node-fetch');
+
+var provider = new ethers.providers.JsonRpcProvider({"url": process.env.API_URL_BASE});
+var signer;
+
+var addr = {
+    "factory": "",
+    "implementation": "",
+    "streamStaker": "",
+    "host": "0x4C073B3baB6d8826b8C5b229f3cfdC1eC6E47E74",
+    "cfa": "0x19ba78B9cDB05A877718841c574325fdB53601bb",
+    "USDbC": "0xd9aAEc86B65D86f6A7B5B1b0c42FFA531710b6CA",
+    "USDbCx": "0x4dB26C973FaE52f43Bd96A8776C2bf1b0DC29556",
+    "USDC": "0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913",
+    "USDCx": "0xD04383398dD2426297da660F9CCA3d439AF9ce1b",
+    "cbETH": "0x2Ae3F1Ec7F1F5012CFEab0185bfc7aa3cf0DEc22",
+    "WETH": "0x4200000000000000000000000000000000000006"
+};
+
+function getWidgetJSON(to) {
+    const widget = {
+        "productDetails": {
+            "name": `Stream Staker`,
+            "description": 'Stream USDC on Base to dollar-cost-average into staked ETH (cbETH)',
+            "imageURI": `https://encycolorpedia.com/emojis/water-wave.svg`,
+            "image": null
+        },
+        "paymentDetails": {
+            "defaultReceiverAddress": "",
+            "paymentOptions": [
+                {
+                    "receiverAddress": to,
+                    "superToken": {
+                        "address": addr.USDCx
+                    },
+                    "chainId": 8453,
+                    "flowRate": {
+                        "amountEther": "1",
+                        "period": "day"
+                    }
+                },
+                {
+                    "receiverAddress": to,
+                    "superToken": {
+                        "address": addr.USDCx
+                    },
+                    "chainId": 8453,
+                    "flowRate": {
+                        "amountEther": "10",
+                        "period": "month"
+                    }
+                },
+                {
+                    "receiverAddress": to,
+                    "superToken": {
+                        "address": addr.USDCx
+                    },
+                    "chainId": 8453,
+                    "flowRate": {
+                        "amountEther": "100",
+                        "period": "month"
+                    }
+                },
+                {
+                    "receiverAddress": to,
+                    "superToken": {
+                        "address": addr.USDCx
+                    },
+                    "chainId": 8453,
+                    "flowRate": {
+                        "amountEther": "500",
+                        "period": "month"
+                    }
+                },
+                {
+                    "receiverAddress": to,
+                    "superToken": {
+                        "address": addr.USDCx
+                    },
+                    "chainId": 8453,
+                    "flowRate": {
+                        "amountEther": "1000",
+                        "period": "month"
+                    }
+                },
+            ]
+        },
+        "layout": "page",
+        "theme": {
+            "typography": {
+                "fontFamily": "'Noto Sans', 'sans-serif'"
+            },
+            "palette": {
+                "mode": "light",
+                "primary": {
+                    "main": "#1DB227"
+                },
+                "secondary": {
+                    "main": "#fff"
+                }
+            },
+            "shape": {
+                "borderRadius": 20
+            },
+            "components": {
+                "MuiStepIcon": {
+                    "styleOverrides": {
+                        "text": {
+                            "fill": "#fff"
+                        }
+                    }
+                },
+                "MuiOutlinedInput": {
+                    "styleOverrides": {
+                        "root": {
+                            "borderRadius": 10
+                        }
+                    }
+                },
+                "MuiButton": {
+                    "styleOverrides": {
+                        "root": {
+                            "borderRadius": 10
+                        }
+                    }
+                }
+            }
+        }
+    };
+    return widget;
+}
+
+async function pinJson(widget) {
+    return new Promise(async (resolve, reject) => {
+        const pinataUri = `https://api.pinata.cloud/pinning/pinJSONToIPFS`;
+        const headers = {
+            'Content-Type': 'application/json',
+            'pinata_api_key': process.env.PINATA_API_KEY,
+            'pinata_secret_api_key': process.env.PINATA_SECRET_API_KEY
+        };
+        var res = await fetch(pinataUri, { 
+            method: 'POST', 
+            headers: headers,
+            body: JSON.stringify(widget)
+        });
+        var pinResult = await res.json(); 
+        console.log(pinResult);
+        resolve(pinResult.IpfsHash);
+    });
+}
+
+function getParams(req, res, next) {
+    var params;
+    if (req.method === 'POST') {
+      params = req.body;
+    } else {
+      params = req.query;
+    }
+    req.q = params;
+    next();
+}
+
+function cors(req, res, next) {
+    res.set('Access-Control-Allow-Origin', '*');
+    if (req.method === 'OPTIONS') {
+      // Send response to OPTIONS requests
+      res.set('Access-Control-Allow-Methods', 'GET, POST');
+      res.set('Access-Control-Allow-Headers', 'Content-Type');
+      res.set('Access-Control-Max-Age', '3600');
+      return res.status(204).send('');
+    } else {
+      // Set CORS headers for the main request
+      res.set('Access-Control-Allow-Origin', '*');
+    }
+    next();
+}
+
+api.use(cors);
+api.use(getParams);
+
+api.get("/api", async function (req, res) {
+    return res.json({"what": "streamstaker.finance", "why": "tba"});
+});
+
+api.post("/api/widget", async function (req, res) {
+    const owner = req.q.owner;
+    
+    // TODO: lookup owner's staker contract address
+
+    const widget = getWidgetJSON(to);
+
+    const cid = await pinJson(widget);
+
+    return res.json({"from": owner, "to": to, "widgetJSON": widget, "widgetUri": `https://checkout.superfluid.finance/${cid}`});
+});
+
+module.exports.api = api;
+
